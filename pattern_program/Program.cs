0c78a3d345af52df0dcb9335b74c114a692522a8
Rajeshkumar Sivaprakasam <rajesh.kumar@goavega.com>
2020-12-24 19:15:12 +0530

    Pattern program added!

---
+++ pattern_program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pattern_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number of Line: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Start printing!");
+            for(int i = 1 ; i <= number; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(j+" ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
